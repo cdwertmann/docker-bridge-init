7b3254268fdb8b6e4ea7184d9f896386c9607276
Christoph Dwertmann <cdwertmann@gmail.com>
2015-03-19 16:51:33 +1100

    check whether DB already exists

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,16 +26,18 @@ until mysql -h mysql_backup -e ";" ; do
   sleep 3
 done
 
-mysql -h mysql_backup << EOF
-CHANGE MASTER TO MASTER_HOST='mysql.novalocal.node.dc1.consul', MASTER_USER='repl',MASTER_PASSWORD='$MYSQL_SLAVE_PW',MASTER_LOG_POS=$pos;
-START SLAVE;
-EOF
+if ! mysql -h mysql_backup -e "use ESC4;"; then
+  mysql -h mysql_backup << EOF
+  CHANGE MASTER TO MASTER_HOST='mysql.novalocal.node.dc1.consul', MASTER_USER='repl',MASTER_PASSWORD='$MYSQL_SLAVE_PW',MASTER_LOG_POS=$pos;
+  START SLAVE;
+  EOF
 
-# create bridge user and add permissions
-mysql -h mysql << EOF
-CREATE DATABASE ESC4;
-GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';
-EOF
+  # create bridge user and add permissions
+  mysql -h mysql << EOF
+  CREATE DATABASE ESC4;
+  GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';
+  EOF
 
-# import bridge initial DB
-wget --no-check-certificate -qO- $INITIAL_SQL_URL | mysql -h mysql ESC4
+  # import bridge initial DB
+  wget --no-check-certificate -qO- $INITIAL_SQL_URL | mysql -h mysql ESC4
+fi
