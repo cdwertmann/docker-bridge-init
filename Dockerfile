6ef165b490f0b81f2e72459cdd7c3b72e280e3e6
Christoph Dwertmann <cdwertmann@gmail.com>
2015-04-01 10:59:00 +1100

    Revert c480444..bbcb154
    This rolls back to commit c480444c6a6bccd41c8758a74e3bddb3fe41fc40.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
-FROM alpine:latest
-RUN apk --update add mysql-client curl bash && rm -rf /var/cache/apk/*
+FROM mysql:5.7
+
+RUN apt-get update && apt-get install -y wget --no-install-recommends && rm -rf /var/lib/apt/lists/*
+
 COPY docker-entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,30 +10,30 @@ protocol=tcp
 EOF
 
 # set up master
-until mysql -h mysql.service.consul -e ";" ; do
+until mysql -h mysql -e ";" ; do
   echo "waiting for connection to database host 'mysql'..."
   sleep 3
 done
 
-if ! mysql -h mysql.service.consul -e "use ESC4;"; then
+if ! mysql -h mysql -e "use ESC4;"; then
   # create bridge user and add permissions
-  mysql -h mysql.service.consul -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
+  mysql -h mysql -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
   # import bridge initial DB
-  curl $INITIAL_SQL_URL | mysql -h mysql.service.consul ESC4
+  wget --no-check-certificate -qO- $INITIAL_SQL_URL | mysql -h mysql ESC4
 fi
 
 # set up slave
-until mysql -h mysql_backup.service.consul -e ";" ; do
+until mysql -h mysql_backup -e ";" ; do
   echo "waiting for connection to database host 'mysql_backup'..."
   sleep 3
 done
 
-pos=`mysql -h mysql.service.consul <<EOF | grep bin | awk '{print $2;}'
+pos=`mysql -h mysql <<EOF | grep bin | awk '{print $2;}'
 GRANT REPLICATION SLAVE ON *.*  TO 'repl'@'mysql-backup.novalocal.node.dc1.consul' IDENTIFIED BY '$MYSQL_SLAVE_PW';
 FLUSH TABLES WITH READ LOCK;SHOW MASTER STATUS;UNLOCK TABLES;
 EOF`
 
-mysql -h mysql_backup.service.consul <<EOF
+mysql -h mysql_backup <<EOF
 STOP SLAVE FOR CHANNEL '';
 RESET SLAVE;
 CHANGE MASTER TO MASTER_HOST='mysql.novalocal.node.dc1.consul', MASTER_USER='repl',MASTER_PASSWORD='$MYSQL_SLAVE_PW',MASTER_LOG_POS=$pos;
