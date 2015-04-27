97af22ea14d24e94982d35be443e81fd1c2a5e6a
Christoph Dwertmann <cdwertmann@gmail.com>
2015-04-27 19:47:06 +1000

    wget fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,8 +20,9 @@ if ! mysql -h mysql -e "use ESC4;"; then
   mysql -h mysql -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
   mysql -h mysql -e "CREATE DATABASE training_ESC4;GRANT ALL PRIVILEGES ON training_ESC4.* To 'training_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
   # import bridge initial DB
-  wget --no-check-certificate -qO- $INITIAL_SQL_URL | mysql -h mysql ESC4
-  wget --no-check-certificate -qO- $INITIAL_SQL_URL | mysql -h mysql training_ESC4
+  wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
+  mysql -h mysql ESC4 < import.sql
+  mysql -h mysql training_ESC4 < import.sql
 fi
 
 # set up slave
