edb2210e99e60394d0e830f62e6ab5ee8bfa5865
Christoph Dwertmann <cdwertmann@gmail.com>
2015-05-29 15:31:37 +1000

    revoke write privilege for tableau

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -17,10 +17,15 @@ done
 
 mysql -h mysql -e "GRANT REPLICATION SLAVE ON *.*  TO 'repl'@'%' IDENTIFIED BY '$MYSQL_SLAVE_PW';"
 
+mysql -h mysql -e "REVOKE ALL PRIVILEGES ON ESC4.* FROM 'tableau'@'%';" 
+mysql -h mysql -e "REVOKE ALL PRIVILEGES ON training_ESC4.* FROM 'tableau'@'%';"
+mysql -h mysql -e "GRANT SELECT ON ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';" 
+mysql -h mysql -e "GRANT SELECT ON training_ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"
+
 if ! mysql -h mysql -e "use ESC4;"; then
   # create bridge user and add permissions
   mysql -h mysql -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -h mysql -e "GRANT ALL PRIVILEGES ON ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"  
+  mysql -h mysql -e "GRANT SELECT ON ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"  
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql -h mysql ESC4 < import.sql
@@ -30,7 +35,7 @@ if ! mysql -h mysql -e "use training_ESC4;"; then
   # create bridge user and add permissions
   mysql -h mysql -e "CREATE DATABASE training_ESC4;GRANT ALL PRIVILEGES ON training_ESC4.* To 'training_rails'@'%' IDENTIFIED BY '$BRIDGEDB_TRAINING_PASSWORD';"
   mysql -h mysql -e "GRANT ALL PRIVILEGES ON training_ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -h mysql -e "GRANT ALL PRIVILEGES ON training_ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"
+  mysql -h mysql -e "GRANT SELECT ON training_ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql -h mysql training_ESC4 < import.sql
