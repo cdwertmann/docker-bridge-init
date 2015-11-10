abfa80afc720531ca63c9b97f1ab478cd9e0c253
Christoph Dwertmann <cdwertmann@gmail.com>
2015-11-10 18:54:46 +1100

    set correct tableau permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -20,7 +20,7 @@ mysql -h mysql -e "GRANT REPLICATION SLAVE ON *.*  TO 'repl'@'%' IDENTIFIED BY '
 if ! mysql -h mysql -e "use ESC4;"; then
   # create bridge user and add permissions
   mysql -h mysql -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -h mysql -e "GRANT SELECT ON ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"  
+  mysql -h mysql -e "SELECT CONCAT(\"GRANT SELECT ON ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";"
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql -h mysql ESC4 < import.sql
@@ -30,7 +30,7 @@ if ! mysql -h mysql -e "use training_ESC4;"; then
   # create bridge user and add permissions
   mysql -h mysql -e "CREATE DATABASE training_ESC4;GRANT ALL PRIVILEGES ON training_ESC4.* To 'training_rails'@'%' IDENTIFIED BY '$BRIDGEDB_TRAINING_PASSWORD';"
   mysql -h mysql -e "GRANT ALL PRIVILEGES ON training_ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -h mysql -e "GRANT SELECT ON training_ESC4.* To 'tableau'@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';"
+  mysql -h mysql -e "SELECT CONCAT(\"GRANT SELECT ON training_ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"training_ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";"
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql -h mysql training_ESC4 < import.sql
