b3e4c92a3a95a1343afc3cf722c9d82fc5d9072d
Christoph Dwertmann <cdwertmann@gmail.com>
2016-01-15 15:28:20 +1100

    set tableau permissions after import

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,22 +12,24 @@ mysql -e "GRANT REPLICATION SLAVE ON *.*  TO 'repl'@'%' IDENTIFIED BY '$MYSQL_SL
 if ! mysql -e "use ESC4;"; then
   # create bridge user and add permissions
   mysql -e "CREATE DATABASE ESC4;GRANT ALL PRIVILEGES ON ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -N -s -r -e "SELECT CONCAT(\"GRANT SELECT ON ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";" > /tmp/sql
-  mysql < /tmp/sql
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql ESC4 < import.sql
+  # create tableau user and set permissions
+  mysql -N -s -r -e "SELECT CONCAT(\"GRANT SELECT ON ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";" > /tmp/sql
+  mysql < /tmp/sql
 fi
 
 if ! mysql -e "use training_ESC4;"; then
   # create bridge user and add permissions
   mysql -e "CREATE DATABASE training_ESC4;GRANT ALL PRIVILEGES ON training_ESC4.* To 'training_rails'@'%' IDENTIFIED BY '$BRIDGEDB_TRAINING_PASSWORD';"
   mysql -e "GRANT ALL PRIVILEGES ON training_ESC4.* To 'esc4_rails'@'%' IDENTIFIED BY '$BRIDGEDB_PASSWORD';"
-  mysql -N -s -r -e "SELECT CONCAT(\"GRANT SELECT ON training_ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"training_ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";" > /tmp/sql
-  mysql < /tmp/sql
   # import bridge initial DB
   wget --no-check-certificate -qO import.sql $INITIAL_SQL_URL
   mysql training_ESC4 < import.sql
+  # create tableau user and set permissions
+  mysql -N -s -r -e "SELECT CONCAT(\"GRANT SELECT ON training_ESC4.\", table_name, \" TO tableau@'%' IDENTIFIED BY '$TABLEAUDB_PASSWORD';\") FROM information_schema.TABLES WHERE table_schema = \"training_ESC4\" AND table_name <> \"jobs\" AND table_name <> \"old_jobs\";" > /tmp/sql
+  mysql < /tmp/sql
 fi
 
 # set up slave
@@ -36,6 +38,6 @@ until mysql -h mysql_backup -e ";" ; do
   sleep 3
 done
 
-if ! mysql -h mysql_backup -e "use ESC4;"; then
-  mysql -h mysql_backup -e "STOP SLAVE;CHANGE MASTER TO MASTER_HOST='mysql', MASTER_USER='repl',MASTER_PASSWORD='$MYSQL_SLAVE_PW',MASTER_LOG_FILE='master-bin.000001',MASTER_LOG_POS=4;START SLAVE;"
+if ! mysql -h $MYSQL_SLAVE_HOST -e "use ESC4;"; then
+  mysql -h $MYSQL_SLAVE_HOST -e "STOP SLAVE;CHANGE MASTER TO MASTER_HOST='mysql', MASTER_USER='repl',MASTER_PASSWORD='$MYSQL_SLAVE_PW',MASTER_LOG_FILE='master-bin.000001',MASTER_LOG_POS=4;START SLAVE;"
 fi
